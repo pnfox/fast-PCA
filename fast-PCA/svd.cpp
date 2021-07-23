95d434cd73dc28566f1d005706556c510f3d9a8d
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2021-07-23 21:07:44 +0100

    Use SVD to perform PCA

--- fast-PCA/svd.cpp
+++ fast-PCA/svd.cpp
@@ -10,22 +10,37 @@
 #include <arrayfire.h>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 using namespace af;
 
 void fast_PCA()
 {
     try {
-        float h_buffer[] = {1, 4, 2, 5, 3, 6};  // host array
-        array in(2, 3, h_buffer);               // copy host data to device
+        array x = randn(20, 5, f32);               // copy host data to device
+        af_print(x);
+
+        array m = mean(x);
+        m = tile(m, x.dims()[0]);
+        array b = x - m;
 
         array u;
         array s_vec;
         array vt;
-        svd(u, s_vec, vt, in);
-
+        svd(u, s_vec, vt, b);
         array s_mat    = diag(s_vec, 0, false);
-        array in_recon = matmul(u, s_mat, vt(seq(2), span));
+
+        // flip signs
+        u = u * (u / sqrt(u*u));
+        vt = vt * (vt / sqrt(vt*vt));
+
+        std::cout << "Performing PCA..." << std::endl;
+        af_print(u);
+        af_print(s_mat);
+        af_print(vt);
+        af_print(u(span, seq(5)));
+        array pca = matmul(u(span, seq(5)), s_mat);
+        af_print(pca);
 
     } catch (af::exception& e) {
         fprintf(stderr, "%s\n", e.what());
