2b797be2a4d61ad45a32137a1accf25a8dcef2b9
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2021-08-17 11:26:02 +0100

    Calculate PCA using Eigen

--- fast-PCA/fast_pca.h
+++ fast-PCA/fast_pca.h
@@ -1,6 +1,5 @@
 #ifndef _FAST_PCA_H_
 #define _FAST_PCA_H_
-#include <arrayfire.h>
 #include <Eigen/Dense>
 #include <fstream>
 #include <string>
@@ -10,12 +9,36 @@
 
 
 
-af::array fast_PCA(af::array x);
-
+// Unfortunately templated Eigen functions need to be in the header
 template <typename Derived>
-Eigen::MatrixXf fast_PCA_eigen(const Eigen::MatrixBase<Derived>& x);
+Eigen::MatrixXf fast_PCA(Eigen::MatrixBase<Derived>& x){
+    try {
+        Eigen::VectorXf m = x.colwise().mean();
+        x.rowwise() -= m.transpose();
 
-af::array read_csv(std::string file_name);
-Eigen::MatrixXf read_csv_eigen(std::string file_name);
+        Eigen::BDCSVD<Eigen::MatrixXf> svd;
+
+        if(x.rows() > x.cols()){
+            svd.compute(x, Eigen::ComputeThinV);
+        } else{
+            svd.compute(x, Eigen::ComputeFullV);
+        }
+
+        Eigen::VectorXf s_vec = svd.singularValues();
+        Eigen::MatrixXf s_mat = s_vec.asDiagonal();
+        
+
+//        Eigen::MatrixXf pca = svd.matrixU() * s_mat;
+        Eigen::MatrixXf pca = x * svd.matrixV();
+
+        return pca;
+
+    } catch (std::exception &e) {
+        fprintf(stderr, "%s\n", e.what());
+        throw;
+    }
+}
+
+Eigen::MatrixXf read_csv(std::string file_name);
 
 #endif
--- fast-PCA/main.cpp
+++ fast-PCA/main.cpp
@@ -3,25 +3,13 @@
 
 int main(int argc, char* argv[]) {
 
-    // Select a device and display arrayfire info
-    af::setDevice(0);
-
-    Eigen::MatrixXf data = read_csv_eigen("../data/data.csv");
-    std::cout << data.rows() << ", " << data.cols() << std::endl;
+    Eigen::MatrixXf data = read_csv("../data/data.csv");
 
     std::cout << "Performing PCA..." << std::endl;
-    Eigen::MatrixXf data2(2,3);
-    data2 << 3, 3, 3, 6, 6, 6;
-    std::cout << data2 << std::endl;
-    
-    Eigen::VectorXf m = data2.colwise().mean();
-    std::cout << m << std::endl;
-    std::cout << m.rows() << ", " << m.cols() << std::endl;
-    data2.rowwise() -= m.transpose();
+   
+    Eigen::MatrixXf pca = fast_PCA(data);
 
-    Eigen::MatrixXf pca = fast_PCA_eigen(data2);
     std::cout << pca << std::endl;
-
 
     return 0;
 }
